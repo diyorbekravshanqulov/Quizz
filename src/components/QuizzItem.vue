cb15efa400741a86e870b38e2fe13988408676f0
diyorbekravshanqulov <diyorbekravshanqulov17@gmail.com>
2024-05-31 09:35:36 +0500

    complate

--- src/components/QuizzItem.vue
+++ src/components/QuizzItem.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="hover:bg-gray-50 p-5 mb-5">
-    <h1 class="text-4xl font-bold mb-5">{{ quizz.question }}</h1>
+    <h1 class="text-4xl font-bold mb-5">
+      {{ quizz.id }}. Hisoblang
+      <span class="font-thin block my-10 mx-10">
+        {{ quizz.question }}
+      </span>
+    </h1>
     <div class="flex flex-col">
       <label
-        :class="
-          quizz.answers[answerIndex] === quizz.userAnswer
-            ? 'bg-slate-900 text-white'
-            : 'hover:bg-primary'
-        "
-        class="p-8 text-2xl"
         v-for="(answer, answerIndex) in quizz.answers"
         :key="answerIndex"
+        :class="[
+          'p-8 text-2xl',
+          quizz.userAnswer === answer
+            ? 'bg-slate-900 text-white'
+            : 'hover:bg-primary',
+        ]"
         :for="`id-${quizz.id}-${answerIndex}`"
       >
         {{ "abcd"[answerIndex] }}) {{ answer }}
@@ -36,24 +41,16 @@ const props = defineProps({
   },
 });
 
-const userAnswer = ref("");
-
-const emit = defineEmits(["user-select-answer", "user-remove-answer"]);
+const userAnswer = ref(props.quizz.userAnswer);
+const emit = defineEmits(["user-select-answer"]);
 
 const selectAnswer = (answerIndex) => {
   const selectedAnswer = props.quizz.answers[answerIndex];
-  if (userAnswer.value === selectedAnswer) {
-    userAnswer.value = "";
-    emit("user-remove-answer", {
-      id: props.quizz.id,
-      userAnswer: "",
-    });
-  } else {
-    userAnswer.value = selectedAnswer;
-    emit("user-select-answer", {
-      id: props.quizz.id,
-      userAnswer: selectedAnswer,
-    });
-  }
+  userAnswer.value = selectedAnswer;
+  // alert("ok");
+  emit("user-select-answer", {
+    id: props.quizz.id,
+    userAnswer: userAnswer.value,
+  });
 };
 </script>
